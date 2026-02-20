f51b7f29658695b0e9fbb9f492c12d327516809a
Joe Spinelli <josephspinelli@yahoo.com>
2026-02-20 01:26:11 -0600

    challenge 1: String reversal set up

---
+++ string-reversal/solution.js
@@ -0,0 +1,13 @@
+// Challenge:
+// 	Given a string, return a new string with the reversed order of characters.
+//
+// 	Examples:
+//
+// reverse('apple') === 'leppa'
+// reverse('hello') === 'olleh'
+// reverse('Greetings!') === '!sgniteerG'
+// You should put your implementation into the reverse function that was defined for you below.  You don't need to call reverse yourself.  Example of what your completed code should look like:
+
+function reverse (str) {
+
+}
