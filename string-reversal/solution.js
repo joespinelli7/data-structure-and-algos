fee966dde61c5d0637f77c4e55276656febdc219
Joe Spinelli <josephspinelli@yahoo.com>
2026-02-23 22:21:20 -0600

    string reversal: My completed solution

--- string-reversal/solution.js
+++ string-reversal/solution.js
@@ -8,6 +8,7 @@
 // reverse('Greetings!') === '!sgniteerG'
 // You should put your implementation into the reverse function that was defined for you below.  You don't need to call reverse yourself.  Example of what your completed code should look like:
 
-function reverse (str) {
-
+// my solution:
+function reverse(str) {
+	return str.split('').reverse().join('');
 }
