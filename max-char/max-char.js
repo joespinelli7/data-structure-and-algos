d0921e0cf626df52688283fd4c7814dc437a8715
Joe Spinelli <josephspinelli@yahoo.com>
2026-05-18 12:10:20 -0500

    max-char: First half of alt solution

--- max-char/max-char.js
+++ max-char/max-char.js
@@ -28,5 +28,15 @@ function maxChar(str) {
 
 // alt solution 1:
 function maxChar(str) {
+	const charMap = {};
 
+	for (let char of str) {
+		if (charMap[char]) {
+			charMap[char]++;
+		} else {
+			charMap[char] = 1;
+		}
+	}
+
+	console.log(charMap);
 }
